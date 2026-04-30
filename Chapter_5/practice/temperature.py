505c9cf6ad990155498ad45ba85ef7b960ab6792
Marema Motaung <marema.motaung@gmail.com>
2026-04-30 19:31:37 +0200

    completed chapter 5

---
+++ Chapter_5/practice/temperature.py
@@ -0,0 +1,27 @@
+# temperature = 20
+# if temperature > 25:
+#     print("It's a hot day")
+
+# if temperature < 40:
+#     print("It's a warm day")
+
+# if temperature == 30:
+#     print("The temperature is exactly 30 degrees")
+
+#Examples with if statements and if else statements
+# color =  'yellow' 
+# if color == 'red':
+#     print("The color is red")
+# else:
+#     print("The color is not red")
+
+#Example with multiple if-elif statements
+grade = 87
+if grade >= 90:     
+    print("You got an A")
+elif grade >= 80:
+    print("You got a B")
+elif grade >= 70:
+    print("You got a C!")
+elif grade >= 60:
+    print("You got a D")
